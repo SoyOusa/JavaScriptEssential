42597b82519151b0680b678945514004229ef9bb
SoyOusa <soyousa31@gmail.com>
2026-05-27 23:49:54 -0400

    add reset button to reset the followes

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -4,6 +4,7 @@ function increaseCount() {
     count++ // Increment the count by 1
     displayCount(); // Display the count
     checkCountValue(); // Check count value and display messages
+    
 }
 
 function displayCount() {
@@ -15,5 +16,11 @@ function checkCountValue() {
     alert("Your Instagram post gained 10 followers! Congratulations!");
   } else if (count === 20) {
     alert("Your Instagram post gained 20 followers! Keep it up!");
-  }
+  }  
+}
+
+function resetCount() {
+    count = 0;
+    displayCount();
+    alert("Your Followers count has been reset");
 }
