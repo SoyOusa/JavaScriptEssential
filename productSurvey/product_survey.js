eb156f664364c35f83cbd3a181e52a99cd87986e
SoyOusa <soyousa31@gmail.com>
2026-05-28 00:41:49 -0400

    add user experience

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -1,6 +1,16 @@
 function submitFeedback() {
-    
+
+    const username = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+    const email = document.getElementById('email').value;
+    const job = document.getElementById('job').value;
+    const designation = document.getElementById('designation').value;
+    const productType = document.getElementById('productType').value;
+    const feedback = document.getElementById('feedbackText').value;
+    const experience = document.getElementById('userExperience').value;
+
     alert('Thank you for your valuable feedback');
+
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
     document.getElementById('userEmail').innerHTML = email;
@@ -8,23 +18,19 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
-    document.getElementById('userInfo').style.display = 'block';
 
+    // Correct display element
+    document.getElementById('displayExperience').innerHTML = experience;
+
+    document.getElementById('userInfo').style.display = 'block';
 }
 
-const username = document.getElementById('name').value;
-const age = document.getElementById('age').value;
-const email = document.getElementById('email').value;
-const job = document.getElementById('job').value;
-const designation = document.getElementById('designation').value;
-const productType = document.getElementById('productType').value;
-const feedback = document.getElementById('feedbackText').value;
+const submitButton = document.getElementById('submitBtn');
 
-const submitButton=document.getElementById('submitBtn');
 submitButton.onclick = submitFeedback;
 
 document.addEventListener('keydown', function(event) {
    if (event.key === 'Enter') {
      submitFeedback();
    }
- });
+});
